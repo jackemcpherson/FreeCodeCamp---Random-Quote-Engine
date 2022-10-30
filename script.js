b142b7163d438a5ec6848096b5c840181f02c84d
Jack McPherson <jackemcpherson@gmail.com>
2022-10-30 11:23:48 +1100

    Moved JS to 'Script.js' and changed API Call from Ajax to Axios

---
+++ script.js
@@ -0,0 +1,15 @@
+function getQuote() {
+    axios.get("https://api.quotable.io/random")
+        .then(response => {
+            result = response.data
+            quoteStr = '"' + result["content"] + '"';
+            authorStr = result["author"];
+            $(".quote-text").text(quoteStr);
+            $(".quote-author").text(authorStr);
+        }).catch(error => console.log(error));
+};
+
+$(document).ready(function () {
+    getQuote();
+})
+
