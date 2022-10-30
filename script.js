2fd540c69d3752a57d88c24a3011d5562d6aa1f3
Jack McPherson <jackemcpherson@gmail.com>
2022-10-30 12:15:45 +1100

    Added twitter button functionality.

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+
+//Call API and Set Quote Text
 function getQuote() {
-    axios.get("https://api.quotable.io/random")
+    return axios.get("https://api.quotable.io/random")
         .then(response => {
             result = response.data
             quoteStr = '"' + result["content"] + '"';
             authorStr = result["author"];
+            //Set Quote Text
             $(".quote-text").text(quoteStr);
+            //Set Author Text
             $(".quote-author").text(authorStr);
+            //Twitter Button
+            $('#tweet-quote').attr(
+                'href',
+                'https://twitter.com/intent/tweet?text=' +
+                  encodeURIComponent(quoteStr + ' - ' + authorStr)
+              );
         }).catch(error => console.log(error));
 };
 
 $(document).ready(function () {
+    //Set Text on Page Load
     getQuote();
+    //Refresh Text on Request
     $("#new-quote").on("click", getQuote);
 });
-
