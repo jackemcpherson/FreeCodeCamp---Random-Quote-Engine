73c0acacea96f83738285b32acfb005c752d2732
Jack McPherson <jackemcpherson@gmail.com>
2023-09-18 17:39:34 +1000

    General Refactor.

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-
-//Call API and Set Quote Text
+// Call API and Set Quote Text
 function getQuote() {
-    return axios.get("https://api.quotable.io/random")
-        .then(response => {
-            result = response.data
-            quoteStr = '"' + result["content"] + '"';
-            authorStr = result["author"];
-            //Set Quote Text
-            $(".quote-text").text(quoteStr);
-            //Set Author Text
-            $(".quote-author").text(authorStr);
-            //Twitter Button
-            $('#tweet-quote').attr(
-                'href',
-                'https://twitter.com/intent/tweet?text=' +
-                  encodeURIComponent(quoteStr + ' - ' + authorStr)
-              );
-        }).catch(error => console.log(error));
-};
+  // Make an API request using Axios
+  axios
+    .get("https://api.quotable.io/random")
+    .then((response) => {
+      const result = response.data; // Extract response data
+      const quoteStr = `"${result["content"]}"`; // Format the quote string
+      const authorStr = result["author"]; // Extract the author
 
-$(document).ready(function () {
-    //Set Text on Page Load
-    getQuote();
-    //Refresh Text on Request
-    $("#new-quote").on("click", getQuote);
+      // Set Quote Text
+      $(".quote-text").text(quoteStr);
+      // Set Author Text
+      $(".quote-author").text(authorStr);
+
+      // Twitter Button: Set href attribute for tweeting the quote
+      $("#tweet-quote").attr(
+        "href",
+        `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+          quoteStr + " - " + authorStr
+        )}`
+      );
+    })
+    .catch((error) => {
+      console.log(error); // Log any errors
+      // Consider adding UI error handling here
+    });
+}
+
+// Initialize when document is ready
+$(document).ready(() => {
+  // Set text on page load
+  getQuote();
+  // Refresh text when "new-quote" button is clicked
+  $("#new-quote").on("click", getQuote);
 });
